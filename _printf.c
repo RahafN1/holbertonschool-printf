1a8d31bb2a59813033c9481a2a7f38e56b4c5227
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-14 14:17:50 +0000

    Fix _printf.c: handle unknown specifiers by printing % and character, fix %! and %K edge cases

--- _printf.c
+++ _printf.c
@@ -2,28 +2,70 @@
 #include <unistd.h>
 
 /**
- * _printf - prints formatted output
- * @format: string containing text and format specifiers
+ * print_char - prints a single character
+ * @args: va_list containing the character to print
+ *
+ * Return: number of characters printed (always 1)
+ */
+int print_char(va_list args)
+{
+	char c;
+
+	c = (char)va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string to print
  *
  * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str;
+	int len;
+
+	len = 0;
+	str = va_arg(args, char *);
+	if (!str)
+		str = "(null)";
+	while (str[len])
+	{
+		write(1, &str[len], 1);
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string with conversion specifiers
+ *
+ * Return: number of characters printed, -1 on error
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	int i = 0;
+	int count;
+	int i;
 
+	count = 0;
+	i = 0;
 	if (!format)
 		return (-1);
-
 	va_start(args, format);
-
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 			if (format[i] == 'c')
 				count += print_char(args);
 			else if (format[i] == 's')
@@ -33,10 +75,11 @@ int _printf(const char *format, ...)
 				write(1, "%", 1);
 				count++;
 			}
-			else if (format[i] == '\0')
+			else
 			{
-				va_end(args);
-				return (-1);
+				write(1, &format[i - 1], 1);
+				write(1, &format[i], 1);
+				count += 2;
 			}
 		}
 		else
@@ -44,11 +87,8 @@ int _printf(const char *format, ...)
 			write(1, &format[i], 1);
 			count++;
 		}
-
 		i++;
 	}
-
 	va_end(args);
-
 	return (count);
 }
