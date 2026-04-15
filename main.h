ceebdd307c3b2dda08600c1986ac844eb8bdbcd7
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-15 10:12:02 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,10 +2,13 @@
 #define MAIN_H
 
 #include <stdarg.h>
-
+#include <unistd.h>
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
+int print_char(char c);
+int print_string(char *str);
 int print_int(int n);
-
+int print_number (int n);
+int print_unsigned (unsigned int n);
+int_octal(unsigned int n)
+int print_hex(unsigned int n,int upper case);
 #endif
