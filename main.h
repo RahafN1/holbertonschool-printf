fde03267d5484829b94b680291b6cb5cdc321c51
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-17 20:16:16 +0000

    Update main.h: add prototypes for print_binary, print_unsigned, print_octal and print_hex with buffer support

--- main.h
+++ main.h
@@ -5,12 +5,12 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_int(va_list args);
-int print_binary(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex(va_list args, int uppercase);
+int print_char(va_list args, char *buffer, int *buf_idx);
+int print_string(va_list args, char *buffer, int *buf_idx);
+int print_int(va_list args, char *buffer, int *buf_idx);
+int print_binary(va_list args, char *buffer, int *buf_idx);
+int print_unsigned(va_list args, char *buffer, int *buf_idx);
+int print_octal(va_list args, char *buffer, int *buf_idx);
+int print_hex(va_list args, char *buffer, int *buf_idx, int uppercase);
 
 #endif
